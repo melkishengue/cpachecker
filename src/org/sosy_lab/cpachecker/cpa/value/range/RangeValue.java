9743f7053ae631ac64ac82ec82568a55b67a10e1
Melchisedek Hengue Touomo <henguemelkispro@yahoo.com>
2020-07-22 14:18:54 +0200

    Added range parser classes

---
+++ src/org/sosy_lab/cpachecker/cpa/value/range/RangeValue.java
@@ -0,0 +1,131 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2020  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.sosy_lab.cpachecker.cpa.value.range;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+
+public class RangeValue {
+  List<String> variables = new ArrayList<>();
+  List<String> variablesFullyQualified = new ArrayList<>();
+  HashMap<String, Object> variablesMap = new HashMap<>();
+  HashMap<String, Object> variablesMapFullyQualified = new HashMap<>();
+  String functionName = "UNKNOWN";
+  boolean isNull;
+  String rawRange;
+
+  public List<String> getVariables() {
+    return variables;
+  }
+
+  public void setVariables(List<String> pVariables) {
+    variables = pVariables;
+  }
+
+  public List<String> getVariablesFullyQualified() {
+    return variablesFullyQualified;
+  }
+
+  public void setVariablesFullyQualified(List<String> pVariablesFullyQualified) {
+    variablesFullyQualified = pVariablesFullyQualified;
+  }
+
+  public HashMap<String, Object> getVariablesMap() {
+    return variablesMap;
+  }
+
+  public void setVariablesMap(HashMap<String, Object> pVariablesMap) {
+    variablesMap = pVariablesMap;
+  }
+
+  public HashMap<String, Object> getVariablesMapFullyQualified() {
+    return variablesMapFullyQualified;
+  }
+
+  public void setVariablesMapFullyQualified(HashMap<String, Object> pVariablesMapFullyQualified) {
+    variablesMapFullyQualified = pVariablesMapFullyQualified;
+  }
+
+  public String getFunctionName() {
+    return functionName;
+  }
+
+  public void setFunctionName(String pFunctionName) {
+    functionName = pFunctionName;
+  }
+
+  public boolean isNull() {
+    return isNull;
+  }
+
+  public void setNull(boolean pIsNull) {
+    isNull = pIsNull;
+  }
+
+  public String getRawRange() {
+    return rawRange;
+  }
+
+  public void setRawRange(String pRawRange) {
+    rawRange = pRawRange;
+  }
+
+  public RangeValue(String rawRange) {
+    this.rawRange = rawRange;
+    this.isNull = rawRange.equals("null");
+
+    if (!this.isNull) {
+      String rawRangeNoBraces =
+          rawRange.replace("(", "").replace(")", "");
+
+      String[] arrOfStr = rawRangeNoBraces.split(" ", 5);
+      for (String a : arrOfStr) {
+        String[] arrOfStrVarValue = a.split("::", 5);
+        this.functionName = arrOfStrVarValue[0];
+
+        String[] arrOfStrVar = arrOfStrVarValue[1].split("=");
+        this.variables.add(arrOfStrVar[0]);
+
+        this.variablesMap.put(arrOfStrVar[0], arrOfStrVar[1]);
+
+        String fullyQualifiedName = this.functionName + "::" + arrOfStrVar[0];
+
+        this.variablesFullyQualified.add(fullyQualifiedName);
+        this.variablesMapFullyQualified.put(fullyQualifiedName, arrOfStrVar[1]);
+      }
+    }
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Scope: " + this.functionName);
+
+    for (Entry<String, Object> entry : this.variablesMap.entrySet()) {
+      sb.append(", " + entry.getKey() + ": " + entry.getValue());
+    }
+
+    sb.append(", Raw: " + this.rawRange);
+    sb.append(System.getProperty("line.separator"));
+    return sb.toString();
+  }
+}
---
+++ src/org/sosy_lab/cpachecker/cpa/value/range/RangeValueInterval.java
@@ -0,0 +1,82 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2020  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.sosy_lab.cpachecker.cpa.value.range;
+
+import java.util.regex.Pattern;
+
+public class RangeValueInterval {
+  boolean isLeftOpen;
+  boolean isRightOpen;
+  RangeValue minRange;
+  RangeValue maxRange;
+
+  public boolean isLeftOpen() {
+    return this.minRange.isNull();
+  }
+
+  public boolean isRightOpen() {
+    return this.maxRange.isNull();
+  }
+
+  public RangeValue getMinRange() {
+    return minRange;
+  }
+
+  public RangeValue getMaxRange() {
+    return maxRange;
+  }
+
+  public RangeValueInterval(String rawRangeValue) {
+    String rawRangeValueNoBraces = rawRangeValue;
+    // remove eventual first and last braces
+    rawRangeValueNoBraces = rawRangeValueNoBraces.substring(0, rawRangeValueNoBraces.length() - 1);
+    rawRangeValueNoBraces = rawRangeValueNoBraces.substring(1, rawRangeValueNoBraces.length());
+
+    String[] arrOfStr = rawRangeValueNoBraces.split(Pattern.quote(","), 5);
+
+    String rawLeftRangeValue = arrOfStr[0].trim();
+    String rawRightRangeValue = arrOfStr[1].trim();
+
+    if (arrOfStr[0].equals("")) {
+      rawLeftRangeValue = "null";
+    }
+
+    if (arrOfStr[1].equals("")) {
+      rawRightRangeValue = "null";
+    }
+
+    RangeValue leftRangeValue = new RangeValue(rawLeftRangeValue);
+    RangeValue rightRangeValue = new RangeValue(rawRightRangeValue);
+
+    this.minRange = leftRangeValue;
+    this.maxRange = rightRangeValue;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+
+    sb.append(this.minRange.toString());
+    sb.append(this.maxRange.toString());
+
+    return sb.toString();
+  }
+
+}
