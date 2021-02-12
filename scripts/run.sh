8223afb93707a6f7efa34192256ad05bf5705e88
Melkis Hengue <henguemelkispro@yahoo.com>
2021-02-12 19:53:24 +0100

    Removed command for rebuilding the application

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
 #  onchange "./src/**/*.java" -- sh scripts/run.sh
-ant build-project 
+# ant build-project 
 # rm -rf output
 clear && clear 
 scripts/cpa.sh -config config/master-thesis-rse-test-pathrange-generation.properties -spec config/properties/unreach-call.prp doc/examples/gcd.c
